6ffbf3ca6ad19bad8b3e10dacabd96e8768537a6
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-04-14 22:38:19 +0100

    random number done

---
+++ random.js
@@ -0,0 +1,8 @@
+/**
+ * Generate a random number between 10 to 20.
+ * --------------------------------------------------------------------
+ * 
+ */
+
+let randomNumber = Math.floor((Math.random()*20)+10);
+console.log(randomNumber);
