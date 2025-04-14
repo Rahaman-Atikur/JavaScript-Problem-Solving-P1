d6a94c3fe518350d91307be9f0a9154f0f2a6973
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-04-14 13:31:19 +0100

    date video solved

---
+++ date.js
@@ -0,0 +1,13 @@
+// const date = new Date('4/14/25');  //String 
+// console.log(date.getMonth());
+// console.log(date.getDay());
+// console.log(date.getFullYear());
+const specificDate = new Date(2091,10,26); //number
+console.log(specificDate);
+specificDate.setDate(4);
+console.log(specificDate);
+specificDate.setMonth(4);
+console.log(specificDate);
+console.log(specificDate.toLocaleString());
+
+
