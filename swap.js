222aa7171734b14ab930706d3b20146ddc4db9af
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-04-14 14:01:43 +0100

    swap solved

---
+++ swap.js
@@ -0,0 +1,16 @@
+/**
+ * Swap between two numbers
+ * ---------------------------------------------------------------------------------
+ * 1.declare a two variable
+ * 2.declare temp variable
+ * 3.
+ */
+let a = 7;
+let b =10;
+let temp;
+ temp = a;
+ a = b;
+ b = temp;
+
+ console. log(a,b);
+
